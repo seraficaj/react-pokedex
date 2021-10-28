69528b5892b4d6741c5cdc66de15789e809dea9e
Jason Serafica <seraficaj@gmail.com>
2021-10-28 13:07:27 -0700

    add h1 to all pokemon page

--- src/Components/AllPokemon.js
+++ src/Components/AllPokemon.js
@@ -4,6 +4,7 @@ import PokemonList from "./PokemonList";
 const AllPokemon = (props) => {
   return (
     <>
+    <h1>All Pokemon</h1>
       <Col xs={6} sm={6} md={8}>
         <PokemonList handleOnclick={props.handleOnclick} pokeArray={props.pokeArray} />
       </Col>
